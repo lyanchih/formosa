76bc8de65f565e20121f6010b898636697aaa0eb
Chung Chih, Hung <lyanchih@gmail.com>
2016-09-24 15:48:59 +0800

    Prepare deploy bash

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEPLOY_BRANCH=${DEPLOY_BRANCH:="gh-pages"}
+DEPLOY_URL=${DEPLOY_URL:="https://lyanchih.github.io/formosa"}
+DEPLOY_ORIGIN_URL=${DEPLOY_ORIGIN_URL:="http://127.0.0.1:1313"}
+DEPLOY_CMD="hugo -D -t hugo-agency-theme"
+
+. /Users/lyanchih/gopath
+
+sed -i.bat '/baseurl = /s,=.*$,= "'$DEPLOY_URL'",' config.toml
+
+$DEPLOY_CMD
+
+git checkout $DEPLOY_BRANCH
+
+mv public/* .
+
+git commit -a -m "Relase"
+
+git push
+
+sed -i '/baseurl = /s,=.*$,= "'$DEPLOY_ORIGIN_URL'",' config.toml
